44ccd28aa4aae4a42f30b06e6f5bf3245e9b24d2
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-25 17:33:25 +0300

    Add default redirect & 404 route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import EmployeesPage from "./pages/EmployeesPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import CvsPage from "./pages/CvsPage";
 import EntitiesPage from "./pages/EntitiesPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Navigate to="/employees" />} />
         <Route path="/employees" element={<EmployeesPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/cvs" element={<CvsPage />} />
         <Route path="/entities" element={<EntitiesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+
+export const NotFoundPage = () => {
+  return (
+    <Stack justifyContent="center" alignItems="center" fontSize="2rem">
+      404: Not Found
+    </Stack>
+  );
+};
