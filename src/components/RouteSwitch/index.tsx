a95af5ef1d7ce3d9a4da5f0ca3148c505497bc77
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-25 17:59:16 +0300

    Move Header to RouteSwitch

--- src/components/RouteSwitch/index.tsx
+++ src/components/RouteSwitch/index.tsx
@@ -4,10 +4,12 @@ import { EmployeesPage } from "../../pages/EmployeesPage";
 import { EntitiesPage } from "../../pages/EntitiesPage";
 import { NotFoundPage } from "../../pages/NotFoundPage";
 import { ProjectsPage } from "../../pages/ProjectsPage";
+import { Header } from "../Header";
 
 export const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/employees" />} />
         <Route path="/employees" element={<EmployeesPage />} />
--- src/pages/EmployeesPage/index.tsx
+++ src/pages/EmployeesPage/index.tsx
@@ -1,10 +1,8 @@
-import { Header } from "../../components/Header";
 import { Container } from "@mui/system";
 
 export const EmployeesPage = () => {
   return (
     <>
-      <Header />
       <Container maxWidth={"md"}>
         <div>TEST</div>
       </Container>
