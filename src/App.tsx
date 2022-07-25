b896f6dba76d8236a5fd5ed2c618393f0d98b9a0
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-25 17:55:08 +0300

    Extract routes from App to RouteSwitch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { EmployeesPage } from "./pages/EmployeesPage";
-import { ProjectsPage } from "./pages/ProjectsPage";
-import { CvsPage } from "./pages/CvsPage";
-import { EntitiesPage } from "./pages/EntitiesPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
+import { RouteSwitch } from "./components/RouteSwitch";
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate to="/employees" />} />
-        <Route path="/employees" element={<EmployeesPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/cvs" element={<CvsPage />} />
-        <Route path="/entities" element={<EntitiesPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
+    <>
+      <RouteSwitch />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/RouteSwitch/index.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CvsPage } from "../../pages/CvsPage";
+import { EmployeesPage } from "../../pages/EmployeesPage";
+import { EntitiesPage } from "../../pages/EntitiesPage";
+import { NotFoundPage } from "../../pages/NotFoundPage";
+import { ProjectsPage } from "../../pages/ProjectsPage";
+
+export const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/employees" />} />
+        <Route path="/employees" element={<EmployeesPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/cvs" element={<CvsPage />} />
+        <Route path="/entities" element={<EntitiesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
