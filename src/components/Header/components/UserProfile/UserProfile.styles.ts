5f999c12693ebe2e1a3495a3c2f318d2c334fc05
murashkoIl <muravey.ilya@mail.ru>
2022-07-26 14:30:19 +0300

    move StyledTranslateIcon width to inline sx

--- src/components/Header/components/UserProfile/UserProfile.styles.ts
+++ src/components/Header/components/UserProfile/UserProfile.styles.ts
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { Grid, Typography, Box } from "@mui/material";
-import TranslateIcon from "@mui/icons-material/Translate";
 
 export const StyledGrid = styled(Grid)({
   display: "flex",
@@ -16,10 +15,6 @@ export const StyledTypography = styled(Typography)({
   "&:hover": {
     color: "#1890FF",
   }
-});
-
-export const StyledTranslateIcon = styled(TranslateIcon)({
-  width: "0.875em"
 });
 
 export const StyledBox = styled(Box)({
