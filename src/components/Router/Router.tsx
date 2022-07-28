df6b453a87041d3c33720b43954a231241c4a936
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-28 13:17:15 +0300

    Remove redundant import

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CvsPage } from "../../pages/CvsPage/CvsPage";
 import { EmployeesPage } from "../../pages/EmployeesPage/EmployeesPage";
 import { EntitiesPage } from "../../pages/EntitiesPage/EntitiesPage";
