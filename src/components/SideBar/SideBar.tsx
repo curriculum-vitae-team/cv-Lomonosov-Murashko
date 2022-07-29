e61ce63dc121c91fa26d6fcf97dc66657130e356
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-07-29 11:36:30 +0300

    Move sidebar links to data file

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,28 +1,7 @@
 import { StyledDiv } from "./SideBar.styles";
-import { ROUTE } from "../../route/route";
 
-import DnsIcon from "@mui/icons-material/Dns";
-import AutoStoriesIcon from "@mui/icons-material/AutoStories";
-import FolderIcon from "@mui/icons-material/Folder";
-import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
-import React from "react";
-import { ISideBarLink } from "./SideBar.types";
 import { SideBarLink } from "./components/SideBarLink";
-
-class SideBarLinkData implements ISideBarLink {
-  constructor(
-    public to: string,
-    public name: string,
-    public Icon: React.ComponentType
-  ) {}
-}
-
-const sideBarLinks: ISideBarLink[] = [
-  new SideBarLinkData(ROUTE.EMPLOYEES, "Employees", EmojiPeopleIcon),
-  new SideBarLinkData(ROUTE.PROJECTS, "Projects", DnsIcon),
-  new SideBarLinkData(ROUTE.CVS, "CVs", AutoStoriesIcon),
-  new SideBarLinkData(ROUTE.ENTITIES, "Entities", FolderIcon),
-];
+import { sideBarLinks } from "./SideBar.data";
 
 export const SideBar = () => {
   return (
