250e2a0111b04ab8072503238c89fc718221c70b
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-08-03 17:32:09 +0300

    Build ProjectInfo boilerplate

--- src/components/ProjectInfo/ProjectInfo.tsx
+++ src/components/ProjectInfo/ProjectInfo.tsx
@@ -17,13 +17,18 @@ import { StyledFieldsetWrapper } from "@components/styled/StyledFieldsetWrapper"
 import { StyledFormActions } from "@components/FormActions/FormActions.styles";
 
 export const ProjectInfo = () => {
-  const { control, handleSubmit, reset } = useForm<IProject>();
-  const { errors } = useFormState({ control });
   const { projectId } = useParams();
+  const project = proj.find(({ id }) => id === projectId)!;
+
+  const { control, handleSubmit, reset } = useForm<IProject>({
+    defaultValues: {
+      name: project.name,
+    },
+  });
+
+  const { errors } = useFormState({ control });
 
   const navigate = useNavigate();
-
-  const project = proj.find(({ id }) => id === projectId)!;
 
   const onSubmit: SubmitHandler<IProject> = (data) => {
     // save employee info
@@ -34,7 +39,7 @@ export const ProjectInfo = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <InfoFormWrapper>
         <StyledFieldsetWrapper>
-          <FormLabel required={true}>First Name</FormLabel>
+          <FormLabel required={true}>Name</FormLabel>
           <Controller
             control={control}
             rules={{ required: "Please, specify the field" }}
@@ -44,74 +49,6 @@ export const ProjectInfo = () => {
                 {...field}
                 error={!!errors.name}
                 helperText={errors.name?.message || " "}
-                size="small"
-              />
-            )}
-          />
-        </StyledFieldsetWrapper>
-
-        <StyledFieldsetWrapper>
-          <FormLabel required={true}>Last Name</FormLabel>
-          <Controller
-            control={control}
-            rules={{
-              required: "Please, specify the field",
-            }}
-            name="lastName"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                error={!!errors.lastName}
-                helperText={errors.lastName?.message || " "}
-                size="small"
-              />
-            )}
-          />
-        </StyledFieldsetWrapper>
-        <StyledFieldsetWrapper>
-          <FormLabel required={true}>Email</FormLabel>
-          <Controller
-            control={control}
-            rules={{ required: "Please, specify the field" }}
-            name="email"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                error={!!errors.email}
-                helperText={errors.email?.message || " "}
-                size="small"
-              />
-            )}
-          />
-        </StyledFieldsetWrapper>
-        <StyledFieldsetWrapper>
-          <FormLabel required={true}>Department</FormLabel>
-
-          <Controller
-            control={control}
-            rules={{ required: "Please, specify the field" }}
-            name="department"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                error={!!errors.department}
-                helperText={errors.department?.message || " "}
-                size="small"
-              />
-            )}
-          />
-        </StyledFieldsetWrapper>
-        <StyledFieldsetWrapper>
-          <FormLabel required={true}>Specialization</FormLabel>
-          <Controller
-            control={control}
-            rules={{ required: "Please, specify the field" }}
-            name="specialization"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                error={!!errors.specialization}
-                helperText={errors.specialization?.message || " "}
                 size="small"
               />
             )}
