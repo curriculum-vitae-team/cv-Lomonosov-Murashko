ea868258f33089d3c823a35c3f7136a336d0e718
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-08-08 13:49:39 +0300

    Fix: margin of form

--- src/components/styled/InfoFormWrapper.ts
+++ src/components/styled/InfoFormWrapper.ts
@@ -5,5 +5,5 @@ export const InfoFormWrapper = styled("div")({
   justifyContent: "space-between",
   alignItems: "center",
   flexWrap: "wrap",
-  marginInline: "0.5em",
+  margin: "1em 0.5em",
 });
