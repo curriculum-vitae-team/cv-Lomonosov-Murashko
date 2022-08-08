cb7b47b006dc7b7b8d54d6b2e5f6e49567d84b58
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-08-08 12:33:29 +0300

    Reat: init Redux store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import employeesReducer from "@features/employees/empoloyeesSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    employees: employeesReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
