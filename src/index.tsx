8c893914c04d4e6005bba66dfa21d7d12ace385e
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-08-11 10:44:16 +0300

    Chore: Set up Apollo and GraphQL connection

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,30 @@ import ReactDOM from "react-dom/client";
 import { App } from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "https://cv-gen-be.herokuapp.com/api/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
 );
