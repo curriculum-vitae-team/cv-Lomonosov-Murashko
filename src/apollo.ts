3ee3581bddeb51fbeeee1c3ccc034796d7f7f2c0
Iaan Lamanosau <iaan.lamanosau@protonmail.com>
2022-08-12 13:03:43 +0300

    Chore: Remove console.log

--- src/apollo.ts
+++ src/apollo.ts
@@ -4,8 +4,6 @@ const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_BACKEND,
 });
 
-console.log(process.env.REACT_APP_GRAPHQL_BACKEND);
-
 export const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
