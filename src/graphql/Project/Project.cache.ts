bf960335df5f6cd864ece762d39f411da5935612
murashkoIl <muravey.ilya@mail.ru>
2022-08-17 14:56:44 +0300

    feat: add delete and update functionality

---
+++ src/graphql/Project/Project.cache.ts
@@ -0,0 +1,20 @@
+import { CacheUpdaterFunction } from "src/types";
+import { DeleteProjectInput, DeleteProjectOutput, ProjectsData } from "./Project.interface";
+import { GET_PROJECTS } from "./Project.queries";
+
+export const deleteProjectCacheUpdate =
+  (id: string): CacheUpdaterFunction<DeleteProjectOutput, DeleteProjectInput> =>
+  (cache, { data }) => {
+    const existingProjects = cache.readQuery<ProjectsData>({ query: GET_PROJECTS });
+
+    if (existingProjects && data?.deleteProject.affected) {
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: {
+          users: existingProjects.projects.filter((project) => project.id !== id),
+        },
+      });
+
+      cache.evict({ id });
+    }
+  };
--- src/graphql/Project/Project.interface.ts
+++ src/graphql/Project/Project.interface.ts
@@ -1,3 +1,52 @@
+/* Queries */
+
+export interface ProjectsData {
+  projects: ProjectInfo[];
+}
+
+/* Mutations */
+
+export interface CreateProjectInput {
+  project: ProjectInput;
+}
+
+export interface CreateProjectOutput {
+  createProject: {
+    id: string;
+    project: ProjectInput;
+  };
+}
+
+export interface DeleteProjectInput {
+  id: string;
+}
+
+export interface DeleteProjectOutput {
+  deleteProject: {
+    affected: number;
+  };
+}
+
+export interface UpdateProjectInput {
+  id: string;
+  project: ProjectInput;
+}
+
+export interface UpdateProjectOutput {
+  updateProject: ProjectInfo;
+}
+
+/* Parts */
+
+export interface ProjectInput {
+  name: string;
+  internal_name: string;
+  description: string;
+  domain: string;
+  start_date: string;
+  end_date: string;
+}
+
 export interface ProjectInfo {
   name: string;
   internal_name: string;
@@ -6,4 +55,13 @@ export interface ProjectInfo {
   start_date: string;
   end_date: string;
   id: string;
+}
+
+/* For table */
+
+export interface Project {
+  id: string;
+  internal_name: string;
+  start_date: string;
+  end_date: string;
 }
