36af6b4cb7fe9492cd4f243e5479c9b5f35328f0
murashkoIl <muravey.ilya@mail.ru>
2022-09-13 16:18:30 +0300

    fix: delete unnecessary code

--- src/context/authContext/authContext.tsx
+++ src/context/authContext/authContext.tsx
@@ -43,8 +43,6 @@ function AuthProvider({ children }: IAuthContextProps) {
     { user, access_token }: AuthUserInfo,
     isMemorized: boolean,
   ) => {
-    console.log("user", user);
-    
     dispatch({
       type: ACTIONS.LOGIN,
       payload: user,
