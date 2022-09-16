379257799581d79986e1f2e239a34182a523bc5e
murashkoIl <muravey.ilya@mail.ru>
2022-09-16 11:14:10 +0300

    fix: auth update

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import {
   AuthFlexContainer,
   AuthWrapper,
@@ -7,23 +7,32 @@ import { ROUTE } from "@constants/route";
 import { Tab } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
 import { StyledTabs } from "./Auth.styles";
+import { AuthContext } from "@context/authContext/authContext";
+import { RedirectPage } from "../RedirectPage";
 
 export const Auth = () => {
   const [currentTab, setCurrentTab] = useState(0);
+  const { user } = useContext(AuthContext);
 
   const handleTabChange = () => {
     currentTab === 0 ? setCurrentTab(1) : setCurrentTab(0);
   };
 
   return (
-    <AuthWrapper>
-      <StyledTabs onChange={handleTabChange} value={currentTab}>
-        <Tab label="Sign In" component={Link} to={ROUTE.SIGN_IN} />
-        <Tab label="Sign Up" component={Link} to={ROUTE.SIGN_UP} />
-      </StyledTabs>
-      <AuthFlexContainer>
-        <Outlet />
-      </AuthFlexContainer>
-    </AuthWrapper>
+    <>
+      {user?.profile ? (
+        <RedirectPage to={ROUTE.EMPLOYEES} />
+      ) : (
+        <AuthWrapper>
+          <StyledTabs onChange={handleTabChange} value={currentTab}>
+            <Tab label="Sign In" component={Link} to={ROUTE.SIGN_IN} />
+            <Tab label="Sign Up" component={Link} to={ROUTE.SIGN_UP} />
+          </StyledTabs>
+          <AuthFlexContainer>
+            <Outlet />
+          </AuthFlexContainer>
+        </AuthWrapper>
+      )}
+    </>
   );
 };
