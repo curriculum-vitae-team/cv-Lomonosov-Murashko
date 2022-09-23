fa3d363f659bcc5e1f4bcbb31fe9516fc1511b05
Iaan Lamanosau <iaan.lamanosau@gmail.com>
2022-09-23 15:26:23 +0300

    Extract LanguageSelector

--- src/components/Header/components/LanguageSelect/LanguageSelect.tsx
+++ src/components/Header/components/LanguageSelect/LanguageSelect.tsx
@@ -1,11 +1,6 @@
-import { MenuItem } from "@mui/material";
-import { withOverlay } from "@src/hoc/withOverlay";
-import { languageStore } from "@src/stores/LanguageStore/LanguageStore";
-import { AppLanguage } from "@src/stores/LanguageStore/LanguageStore.types";
-import { observer } from "mobx-react-lite";
 import { useState } from "react";
-import { languages } from "./LanguageSelect.data";
-import { StyledMenu, StyledTranslateIcon } from "./LanguageSelect.styles";
+import { StyledTranslateIcon } from "./LanguageSelect.styles";
+import { LanguageSelector } from "../LanguageSelector";
 
 export const LanguageSelect = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,30 +20,3 @@ export const LanguageSelect = () => {
     </>
   );
 };
-
-const LanguageSelector = withOverlay(
-  observer(({ open }: { open: boolean }) => {
-    const { setLanguage } = languageStore;
-
-    const handleLanguageChange = (lang: AppLanguage) => {
-      return function () {
-        setLanguage(lang);
-      };
-    };
-
-    return (
-      <StyledMenu open={open}>
-        {languages.map((lang) => (
-          <MenuItem
-            disabled={lang === languageStore.language$}
-            sx={{ width: "100%" }}
-            onClick={handleLanguageChange(lang)}
-            key={lang}
-          >
-            {lang}
-          </MenuItem>
-        ))}
-      </StyledMenu>
-    );
-  }),
-);
